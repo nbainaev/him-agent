c152b15b4bb42b181ff2c31a4c12f4fa7a6f7e42
Petr Kuderov <pkuderov@gmail.com>
2022-04-17 23:12:42 +0300

    fix hima run_agents script after copyright notice adding

--- hima/experiments/hima/scripts/run_agents.py
+++ hima/experiments/hima/scripts/run_agents.py
@@ -1,14 +1,15 @@
-if __name__ == '__main__':
-    import subprocess
-    import argparse
-    #  Copyright (c) 2022 Autonomous Non-Profit Organization "Artificial Intelligence Research
 #  Copyright (c) 2022 Autonomous Non-Profit Organization "Artificial Intelligence Research
 #  Institute" (AIRI); Moscow Institute of Physics and Technology (National Research University).
 #  All rights reserved.
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
 
-parser = argparse.ArgumentParser()
+
+if __name__ == '__main__':
+    import subprocess
+    import argparse
+
+    parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--n_processes", type=int, default=1)
     parser.add_argument("-c", "--command", type=str, default='')
 
