5f40bea30263affaebdd7d4a809d3b0d62dbc9d0
gsys <zhekauran@gmail.com>
2022-05-02 16:05:30 +0300

    update experiments

--- hima/experiments/htm/tm_sequence_test.py
+++ hima/experiments/htm/tm_sequence_test.py
@@ -6,6 +6,10 @@
 
 import numpy as np
 from hima.envs.mpg import MarkovProcessGrammar
+from hima.common.sdr_encoders import IntBucketEncoder
+from hima.modules.htm.belief_tm import NaiveBayesTM
+
+import wandb
 
 
 def main():
@@ -30,6 +34,12 @@ def main():
         [0, 0, 0, 0, 0, 0, 0, 'E'],
     ]
 
+    char_to_number = {'B': 0, 'P': 1, 'T': 2, 'V': 3, 'S': 4, 'X': 5, 'E': 6}
+
+    bucket_size = 3
+    seed = 32342
+    encoder = IntBucketEncoder(len(char_to_number), bucket_size)
+
     mpg = MarkovProcessGrammar(
         8,
         transitions,
@@ -37,12 +47,37 @@ def main():
         initial_state=0,
         terminal_state=7,
         autoreset=False,
-        seed=432
+        seed=seed
+    )
+    print(f'n_columns: {encoder.output_sdr_size}')
+    tm = NaiveBayesTM(
+        encoder.output_sdr_size,
+        cells_per_column=8,
+        max_segments_per_cell=3,
+        max_receptive_field_size=-1,
+        w_lr=0.1,
+        w_punish=0.1,
+        theta_lr=0.1,
+        b_lr=0.1,
+        seed=seed
     )
 
-    for i in range(10):
+    logger = wandb.init(
+        project='test_belief_tm', entity='hauska',
+        config=dict(
+            seed=seed,
+            bucket_size=bucket_size
+        )
+    )
+
+    for i in range(1000):
         mpg.reset()
+        tm.reset()
+
         word = []
+        surprise = []
+        anomaly = []
+
         while True:
             letter = mpg.next_state()
 
@@ -51,7 +86,21 @@ def main():
             else:
                 break
 
-        print(i, ''.join(word))
+            tm.set_active_columns(encoder.encode(char_to_number[letter]))
+            tm.activate_cells(learn=True)
+
+            surprise.append(min(200.0, tm.surprise))
+            anomaly.append(tm.anomaly)
+
+            tm.activate_dendrites()
+            tm.predict_cells()
+
+        logger.log({
+            'surprise': np.array(surprise)[1:].mean(),
+            'anomaly': np.array(anomaly)[1:].mean()
+        })
+
+    ...
 
 
 if __name__ == '__main__':
