824d4e116e7cca07ff937db203022a5969ab0c51
Petr Kuderov <pkuderov@gmail.com>
2022-05-22 01:58:01 +0300

    add run setup data class for states experiment

--- hima/experiments/temporal_pooling/config_resolvers.py
+++ hima/experiments/temporal_pooling/config_resolvers.py
@@ -3,6 +3,7 @@
 #  All rights reserved.
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+from typing import Union
 
 from hima.common.config_utils import (
     extracted_type, resolve_init_params, extracted,
@@ -141,12 +142,18 @@ def resolve_tm_connections_region(connections_config, sds, suffix):
     }
 
 
-def resolve_run_setup(config: dict, run_setup_config):
+def resolve_run_setup(config: dict, run_setup_config: Union[dict, str], experiment_type: str):
     if isinstance(run_setup_config, str):
         run_setup_config = config['run_setups'][run_setup_config]
 
-    from hima.experiments.temporal_pooling.new.test_on_policies import RunSetup
-    return RunSetup(**run_setup_config)
+    if experiment_type == 'policies':
+        from hima.experiments.temporal_pooling.new.test_on_policies import RunSetup
+        return RunSetup(**run_setup_config)
+    elif experiment_type == 'observations':
+        from hima.experiments.temporal_pooling.new.test_on_states import RunSetup
+        return RunSetup(**run_setup_config)
+    else:
+        KeyError(f'Experiment type {experiment_type} is not supported')
 
 
 def resolve_data_generator(config: dict, **induction_registry):
