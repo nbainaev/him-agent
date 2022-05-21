994584f53c434640d100a68ca0e74daa07683366
Petr Kuderov <pkuderov@gmail.com>
2022-05-22 02:02:08 +0300

    wip: add states dataset building, run setup and its resolving

--- hima/experiments/temporal_pooling/new/test_on_states.py
+++ hima/experiments/temporal_pooling/new/test_on_states.py
@@ -3,14 +3,21 @@
 #  All rights reserved.
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+from typing import Optional, Union
 
 import numpy as np
 import wandb
 from htm.bindings.sdr import SDR
+from wandb.sdk.wandb_run import Run
 
 from hima.common.config_utils import TConfig
 from hima.common.run_utils import Runner
-from hima.experiments.temporal_pooling.config_resolvers import resolve_data_generator
+from hima.common.sds import Sds
+from hima.common.utils import isnone
+from hima.experiments.temporal_pooling.config_resolvers import (
+    resolve_data_generator,
+    resolve_run_setup
+)
 from hima.experiments.temporal_pooling.metrics import sdrs_similarity
 from hima.experiments.temporal_pooling.test_on_policies import (
     resolve_tp, ExperimentStats,
@@ -27,30 +34,59 @@ def similarity_matrix(representations: list):
     return matrix
 
 
-class ObservationsExperiment(Runner):
+class RunSetup:
+    n_sequences: Optional[int]
+    n_observations_per_sequence: Optional[int]
+    sequence_repeats: int
     epochs: int
 
-    stats: ExperimentStats
+    tp_output_sds: Sds
 
-    def __init__(self, config: TConfig,
-                 temporal_pooler: str,
-                 epochs: int,
-                 rotations_per_room,
-                 **kwargs):
-        super().__init__(config, **config)
-        self.temporal_pooler = temporal_pooler
+    def __init__(
+            self, sequence_repeats: int, epochs: int, tp_output_sds: Sds.TShortNotation,
+            n_sequences: Optional[int] = None, n_observations_per_sequence: Optional[int] = None
+    ):
+        self.n_sequences = n_sequences
+        self.n_observations_per_sequence = n_observations_per_sequence
         self.epochs = epochs
-        self.rotations_per_room = rotations_per_room
+        self.policy_repeats = sequence_repeats
+        self.tp_output_sds = Sds(short_notation=tp_output_sds)
+
+
+class ObservationsExperiment(Runner):
+    config: TConfig
+    logger: Optional[Run]
+
+    seed: int
+    run_setup: RunSetup
+
+    def __init__(
+            self, config: TConfig, run_setup: Union[dict, str], seed: int,
+            # epochs: int,
+            # rotations_per_room,
+            **_
+    ):
+        super().__init__(config, **config)
+        self.seed = seed
+        self.run_setup = resolve_run_setup(config, run_setup, experiment_type='observations')
+        # self.temporal_pooler = temporal_pooler
         print('==> Init')
         self.data_generator = resolve_data_generator(config)
-        self.temporal_memory = ClassicApicalTemporalMemory(**config['apical_temporal_memory'])
-        self.temporal_pooler = resolve_tp(
-            config, temporal_pooler, temporal_memory=self.temporal_memory
+        input_block = self.data_generator.generate_data(
+            n_sequences=self.run_setup.n_sequences,
+            n_observations_per_sequence=self.run_setup.n_observations_per_sequence
         )
-
-        self.stats = ExperimentStats(self.temporal_pooler)
+        print(input_block.output_sds)
+        print(input_block.n_sequences, input_block.n_observations_per_sequence)
+        # self.temporal_memory = ClassicApicalTemporalMemory(**config['apical_temporal_memory'])
+        # self.temporal_pooler = resolve_tp(
+        #     config, temporal_pooler, temporal_memory=self.temporal_memory
+        # )
+        #
+        # self.stats = ExperimentStats(self.temporal_pooler)
 
     def run(self):
+        return
         print('==> Generate observations')
         observations = self.data_generator.generate_data()
 
@@ -71,7 +107,6 @@ class ObservationsExperiment(Runner):
         self.logger.log({
             'similarities': wandb.Image(similarity_cmp(input_similarity_matrix, output_similarity_matrix))
         })
-
 
     def train_epoch(self, observations):
         representations = []
