probably fix of the normalization change

hima/experiments/temporal_pooling/new/metrics.py
@@ -302,7 +302,7 @@ def point_similarity(p: np.ndarray, q: np.ndarray, sds: Sds = None) -> float:
 # ==================== Errors ====================
 def standardize_sample_distribution(x: np.ndarray) -> np.ndarray:
     unbiased_x = x - np.min(x)
-    return safe_divide(unbiased_x, np.max(x) - np.min(x))
+    return safe_divide(unbiased_x, np.max(x) - np.min(x), default=unbiased_x)
 
 
 def mean_absolute_error(x: np.ndarray, y: np.ndarray) -> float: