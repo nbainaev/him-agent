e0be5171cb4ed48d4767bdb4e21d962669d9454f
Petr Kuderov <pkuderov@gmail.com>
2022-08-12 20:37:20 +0300

    resolve blocks connection sds configs

--- hima/experiments/temporal_pooling/blocks/builder.py
+++ hima/experiments/temporal_pooling/blocks/builder.py
@@ -6,10 +6,12 @@
 from hima.common.config_utils import (
     extracted_family, resolve_nested_configs, TConfig
 )
+from hima.experiments.temporal_pooling.blocks.base_block import BlocksConnection, Block
 from hima.experiments.temporal_pooling.blocks.dataset_resolver import resolve_data_generator_new
+from hima.experiments.temporal_pooling.blocks.sp import resolve_sp_new
 
 
-def build_block(config: TConfig, block_name: str, **induction_registry):
+def build_block(config: TConfig, block_id: int, block_name: str, **induction_registry) -> Block:
     block_config = config['blocks'][block_name]
 
     block_config, block_family = extracted_family(block_config)
@@ -17,18 +19,26 @@ def build_block(config: TConfig, block_name: str, **induction_registry):
 
     family_registry = config[block_family]
 
-    print(family_registry)
     block_config = resolve_nested_configs(family_registry, config=block_config)
-    print(block_config)
-    return _resolve_block(config, block_config, block_family, **induction_registry)
+    return _resolve_block(
+        config, block_config, block_family, block_id, block_name, **induction_registry
+    )
 
 
 def _resolve_block(
-        global_config: TConfig, block_config: TConfig, family: str, **induction_registry
-):
+        global_config: TConfig, block_config: TConfig,
+        family: str, block_id: int, block_name: str,
+        **induction_registry
+) -> Block:
     if family == 'generator':
-        return resolve_data_generator_new(global_config, block_config, **induction_registry)
+        return resolve_data_generator_new(
+            global_config, block_config, block_id, block_name, **induction_registry
+        )
+    elif family == 'spatial_pooler':
+        return resolve_sp_new(block_config, block_id, block_name, **induction_registry)
 
 
 def build_connection(connection_config: TConfig, blocks: dict):
-    pass
+    connection = BlocksConnection(block_registry=blocks, **connection_config)
+    connection.align_dimensions()
+    print(connection.src_stream, connection.dst_stream)
