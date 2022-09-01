1af2f8c77846abff3017608e38c74351ef785aaf
Petr Kuderov <pkuderov@gmail.com>
2022-09-01 17:26:21 +0300

    remove incomplete things from old experiments and vice versa part 2

--- hima/experiments/temporal_pooling/blocks/dataset_resolver.py
+++ hima/experiments/temporal_pooling/blocks/dataset_resolver.py
@@ -4,7 +4,6 @@
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
 from hima.common.config_utils import extracted_type, resolve_init_params, TConfig
-from hima.experiments.temporal_pooling.stats_config import StatsMetricsConfig
 
 
 def resolve_data_generator(config: TConfig, **induction_registry):
@@ -19,28 +18,6 @@ def resolve_data_generator(config: TConfig, **induction_registry):
         return AAIRotationsGenerator(**generator_config)
     else:
         raise KeyError(f'{generator_type} is not supported')
-
-
-def resolve_data_generator_new(
-        global_config: TConfig, generator_config: TConfig,
-        block_id: int, block_name: str,
-        n_sequences: int, stats_config: StatsMetricsConfig,
-        **induction_registry
-):
-    generator_config, generator_type = extracted_type(generator_config)
-
-    if generator_type == 'synthetic_sequences':
-        from hima.experiments.temporal_pooling.blocks.dataset_synth_sequences import (
-            SyntheticSequencesGenerator
-        )
-        generator_config = resolve_init_params(generator_config, **induction_registry)
-        generator = SyntheticSequencesGenerator(global_config, **generator_config)
-    else:
-        raise KeyError(f'{generator_type} is not supported')
-
-    sequences = generator.generate_sequences(n_sequences)
-    generator_block = generator.build_block(block_id, block_name, sequences, stats_config)
-    return generator_block
 
 
 def resolve_encoder(
--- hima/experiments/temporal_pooling/new/config_resolvers.py
+++ hima/experiments/temporal_pooling/new/config_resolvers.py
@@ -10,14 +10,8 @@ def resolve_run_setup(config: dict, run_setup_config: Union[dict, str], experime
     if isinstance(run_setup_config, str):
         run_setup_config = config['run_setups'][run_setup_config]
 
-    if experiment_type == 'policies':
-        from hima.experiments.temporal_pooling.test_on_policies import RunSetup
-        return RunSetup(**run_setup_config)
-    elif experiment_type == 'observations':
-        from hima.experiments.temporal_pooling.test_on_states import RunSetup
-        return RunSetup(**run_setup_config)
-    elif experiment_type == 'layered':
-        from hima.experiments.temporal_pooling.test_on_obs_layered import RunSetup
+    if experiment_type == 'layered':
+        from hima.experiments.temporal_pooling.new.test_on_obs_layered import RunSetup
         return RunSetup(**run_setup_config)
     else:
         KeyError(f'Experiment type {experiment_type} is not supported')
