c9ab83f8b42050548918cae38d0fc7d0d28c18c0
Petr Kuderov <pkuderov@gmail.com>
2022-12-08 06:48:04 +0300

    remove deprecated experiments from run registry

--- hima/experiments/temporal_pooling/run.py
+++ hima/experiments/temporal_pooling/run.py
@@ -6,16 +6,12 @@
 
 from hima.common.run.entrypoint import run_experiment, get_run_command_arg_parser
 from hima.experiments.temporal_pooling.test_on_obs_layered import ObservationsLayeredExperiment
-from hima.experiments.temporal_pooling._depr.test_on_policies import PoliciesExperiment
-from hima.experiments.temporal_pooling._depr.test_on_states import ObservationsExperiment
 
 
 if __name__ == '__main__':
     run_experiment(
         run_command_parser=get_run_command_arg_parser(),
         experiment_runner_registry={
-            'tp.policy': PoliciesExperiment,
-            'tp.observations': ObservationsExperiment,
             'tp.layered': ObservationsLayeredExperiment,
         }
     )
---
+++ hima/experiments/temporal_pooling/run_progress.py
@@ -0,0 +1,19 @@
+#  Copyright (c) 2022 Autonomous Non-Profit Organization "Artificial Intelligence Research
+#  Institute" (AIRI); Moscow Institute of Physics and Technology (National Research University).
+#  All rights reserved.
+#
+#  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+
+class RunProgress:
+    epoch: int
+    step: int
+
+    def __init__(self):
+        self.epoch = -1
+        self.step = -1
+
+    def next_epoch(self):
+        self.epoch += 1
+
+    def next_step(self):
+        self.step += 1
