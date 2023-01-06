70307c08e0b4f79f8173978c5f43c8b6e6198fd4
gsys <zhekauran@gmail.com>
2023-01-06 09:23:42 +0300

    fix bug

--- hima/modules/htm/spatial_pooler.py
+++ hima/modules/htm/spatial_pooler.py
@@ -1018,7 +1018,7 @@ class SPDecoder:
         if update:
             self._update_receptive_fields()
 
-        log_product = np.dot(self.receptive_fields.T, np.log(cell_probs))
+        log_product = np.dot(self.receptive_fields.T, np.log(1 - cell_probs))
         return 1 - np.exp(log_product)
 
     def _update_receptive_fields(self):
