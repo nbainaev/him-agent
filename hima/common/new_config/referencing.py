8f36ee928ed95ea5d9b20ab3f6cefb999f5aa2c0
Petr Kuderov <pkuderov@gmail.com>
2023-01-13 18:48:44 +0300

    document sub-config referencing resolvers

--- hima/common/new_config/referencing.py
+++ hima/common/new_config/referencing.py
@@ -6,7 +6,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any
+from typing import Any, Type
 
 from hima.common.new_config.base import TConfig, extracted, TKeyPath
 from hima.common.run.entrypoint import read_config
@@ -31,8 +31,8 @@ _BASE_CONFIG_KEY = '_base_'
 
 
 # ==================== config meta info extraction ====================
-def extracted_type(config: TConfig) -> tuple[TConfig, str | None]:
-    """Extracts the type using the type hinting convention for configs."""
+def extracted_type_tag(config: TConfig) -> tuple[TConfig, str | None]:
+    """Extracts the type tag using the type hinting convention for configs."""
     return extracted(config, _TYPE_KEY)
 
 
@@ -41,8 +41,46 @@ def extracted_base_config(config: TConfig) -> tuple[TConfig, str | None]:
     return extracted(config, _BASE_CONFIG_KEY)
 
 
-# ==================== resolve config references ====================
-class ConfigReferenceResolver:
+class ConfigResolver:
+    """
+    ConfigResolver is a class that resolves the config of an object or any sub-config in general.
+
+    In most cases object constructors expect a dictionary config with named args. For dict configs
+    we support the "_base_" meta key that allows to reference another dict config to take it as
+    base config and override it with the current dict. Such referencing is recursive, i.e. the base
+    config may also have a "_base_" key, and so on.
+
+    We treat "_base_" as a dot-separated key path to the base config, i.e. path containing a
+    sequence of keys that are used to recursively select subconfigs from the global config.
+    For example "path.to.config" means following structure:
+    path:
+        to:
+            config: <base config>
+
+    If the key path starts with "$" it means that the first key of the path is a name (without
+    extension) of the config file to load. The tail of the path is used to select the base config
+    from the loaded config. For example "$another_config.path.to.config" means the same as above,
+    but the base config is loaded from the "another_config.yaml" file in the same folder as the
+    current global config file.
+
+    To prevent repeated loading of the same external config, the whole loaded config is added to
+    the global config under its root key (e.g. "$another_config"), so the key path resolution
+    works with only "local" key paths as is.
+
+    The `default_base_path` is used to resolve the key path when the key path is a single key.
+    In this case provided key is treated as a key in the same parent collection as the last key
+    of the `default_base_path` key path. For example, if the `default_base_path` is
+    "path.to.obj1_config" and the key path is "obj2_config", then it is treated as:
+    path:
+        to:
+            obj1_config:
+            obj2_config: <base config>
+
+    In several cases config is a list (e.g. if object is a list or tuple). As they don't have
+    named args, we don't support referencing with "_base_" key.
+
+    However, for both cases we support direct referencing with a single string key path.
+    """
     global_config: TConfig
     global_config_path: Path
 
@@ -50,15 +88,18 @@ class ConfigReferenceResolver:
         self.global_config = global_config
         self.global_config_path = global_config_path
 
-    def resolve_config(
-            self,
-            config: TConfig | list | str | None,
+    def resolve(
+            self, config: TConfig | str | None,
             *,
-            object_type: type,
+            config_type: Type[dict | list],
             config_path: str = None,
     ) -> TConfig:
+        """
+        Resolves the config of an object or any sub-config in general.
+        See the class docstring for details.
+        """
         if config is None:
-            return object_type()
+            return config_type()
 
         if isinstance(config, str):
             # it is a reference only
@@ -77,9 +118,9 @@ class ConfigReferenceResolver:
             base_config = self._resolve_reference(reference_path, default_base_path=config_path)
 
             # recursively resolve base config the same way as current
-            resolved_base_config = self.resolve_config(
+            resolved_base_config = self.resolve(
                 base_config,
-                object_type=object_type,
+                config_type=config_type,
                 config_path=reference_path,
             )
             # by the implementation we override the innermost base config with each outer one
@@ -89,6 +130,10 @@ class ConfigReferenceResolver:
         return config
 
     def _resolve_reference(self, config_path: str, *, default_base_path: str) -> TConfig:
+        """
+        Resolves the reference to the base config and returns it raw.
+        See the class docstring for details.
+        """
         key_path: TKeyPath = config_path.split('.')
 
         if key_path[0].startswith('$') and key_path[0] not in self.global_config:
@@ -104,6 +149,10 @@ class ConfigReferenceResolver:
         return self._select_by_key_path(key_path)
 
     def _load_external_config(self, reference_root_key: str):
+        """
+        Loads the external config and adds it to the global config.
+        See the class docstring for details.
+        """
         # key: $filename_without_extension
         file_stem = reference_root_key[1:]
         config_filepath = self.global_config_path.with_stem(file_stem)
@@ -112,6 +161,10 @@ class ConfigReferenceResolver:
         self.global_config[reference_root_key] = read_config(config_filepath)
 
     def _select_by_key_path(self, key_path: TKeyPath) -> Any:
+        """
+        Traverses global config and selects a sub-config by the provided key path.
+        See the class docstring for details.
+        """
         sub_config = self.global_config
         for key_token in key_path:
             sub_config = sub_config[key_token]
