08f4a4b0946b42883d589ef99093f5858c73ab76
ZhekaHauska <zhekauran@gmail.com>
2023-01-30 13:14:43 +0300

    add LSTM iterative baseline

---
+++ hima/modules/baselines/lstm.py
@@ -0,0 +1,121 @@
+#  Copyright (c) 2022 Autonomous Non-Profit Organization "Artificial Intelligence Research
+#  Institute" (AIRI); Moscow Institute of Physics and Technology (National Research University).
+#  All rights reserved.
+#
+#  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+torch.autograd.set_detect_anomaly(True)
+
+
+class LSTMIterative:
+    def __init__(
+            self,
+            n_obs_states,
+            n_hidden_states,
+            lr=2e-3,
+            seed=None
+    ):
+        self.n_obs_states = n_obs_states
+        self.n_hidden_states = n_hidden_states
+        self.lr = lr
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.lstm = LSTMWM(
+            input_size=n_obs_states,
+            hidden_size=n_hidden_states
+        ).to(self.device)
+
+        self.prediction = torch.zeros(self.n_obs_states, device=self.device)
+
+        self.loss_function = nn.BCELoss()
+        self.optimizer = optim.RMSprop(self.lstm.parameters(), lr=self.lr)
+
+        torch.manual_seed(seed)
+        self._rng = np.random.default_rng(seed)
+
+    def observe(self, obs, learn=True):
+        dense_obs = np.zeros(self.n_obs_states, dtype='float32')
+        dense_obs[obs] = 1
+
+        dense_obs = torch.from_numpy(dense_obs).to(self.device)
+
+        if learn:
+            loss = self.loss_function(self.prediction, dense_obs)
+            if loss.requires_grad:
+                loss.backward(retain_graph=True)
+
+            self.prediction = self.lstm(dense_obs)
+        else:
+            with torch.no_grad():
+                self.prediction = self.lstm(dense_obs)
+
+    def reset(self):
+        self.optimizer.step()
+        self.prediction = torch.zeros(self.n_obs_states, device=self.device)
+        self.lstm.message = (
+            torch.zeros(self.n_hidden_states, device=self.device),
+            torch.zeros(self.n_hidden_states, device=self.device),
+        )
+        self.lstm.zero_grad()
+
+    def n_step_prediction(self, steps, mc_iterations=100):
+        dist_curr_step = self.prediction.cpu().detach().numpy()
+        dist_next_step = np.zeros(self.n_obs_states)
+        for step in range(steps):
+            for i in range(mc_iterations):
+                # sample observation from prediction density
+                gamma = self._rng.random(size=self.n_obs_states)
+                obs = np.flatnonzero(gamma < dist_curr_step)
+                dense_obs = np.zeros(self.n_obs_states, dtype='float32')
+                dense_obs[obs] = 1
+                dense_obs = torch.from_numpy(dense_obs).to(self.device)
+                # predict distribution
+                with torch.no_grad():
+                    prediction = self.lstm(dense_obs).cpu().detach().numpy()
+                # add to average
+                dist_next_step += 1 / (i + 1) * (prediction - dist_next_step)
+
+            dist_curr_step = np.copy(dist_next_step)
+            dist_next_step = np.zeros_like(dist_curr_step)
+
+        self.prediction = torch.from_numpy(dist_curr_step).to(self.device)
+
+        return dist_curr_step
+
+
+class LSTMWM(nn.Module):
+    def __init__(
+            self,
+            input_size,
+            hidden_size
+    ):
+        super(LSTMWM, self).__init__()
+
+        self.n_obs_states = input_size
+        self.n_hidden_states = hidden_size
+
+        self.lstm = nn.LSTMCell(
+            input_size=input_size,
+            hidden_size=hidden_size
+        )
+
+        # The linear layer that maps from hidden state space back to obs space
+        self.hidden2obs = nn.Linear(
+            hidden_size,
+            input_size
+        )
+
+        self.message = (
+            torch.zeros(self.n_hidden_states),
+            torch.zeros(self.n_hidden_states)
+        )
+
+    def forward(self, obs):
+        self.message = self.lstm(obs, self.message)
+        prediction_logit = self.hidden2obs(self.message[0])
+        prediction = torch.sigmoid(prediction_logit)
+        return prediction
---
+++ hima/modules/baselines/tests/lstmwm.py
@@ -0,0 +1,30 @@
+#  Copyright (c) 2022 Autonomous Non-Profit Organization "Artificial Intelligence Research
+#  Institute" (AIRI); Moscow Institute of Physics and Technology (National Research University).
+#  All rights reserved.
+#
+#  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+from hima.modules.baselines.lstm import LSTMWM
+import torch
+import numpy as np
+
+
+if __name__ == '__main__':
+    device = 'cpu'
+    lstm = LSTMWM(
+        8, 10
+    ).to(device)
+
+    prediction = torch.zeros(8, device=device)
+    observations = np.random.randint(8, size=10)
+    loss_function = torch.nn.BCELoss()
+
+    for obs in observations:
+        dense_obs = np.zeros(8, dtype='float32')
+        dense_obs[obs] = 1
+        dense_obs = torch.from_numpy(dense_obs).to(device)
+
+        loss = loss_function(prediction, dense_obs)
+        if loss.requires_grad:
+            loss.backward(retain_graph=True)
+
+        prediction = lstm(dense_obs)
