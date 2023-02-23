4769903b5749fe2d4c54bdd7d36ed2f24062d35e
Petr Kuderov <pkuderov@gmail.com>
2023-02-23 14:50:43 +0300

    rework synth dataset building, remove old resolvers, refactor unified type resolver

--- hima/experiments/temporal_pooling/blocks/dataset_synth_sequences.py
+++ hima/experiments/temporal_pooling/blocks/dataset_synth_sequences.py
@@ -3,143 +3,62 @@
 #  All rights reserved.
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
-from typing import Iterator
-
-import numpy as np
-from numpy.random import Generator
+import dataclasses
+from typing import Iterator, Any
 
 from hima.common.config.base import TConfig
 from hima.common.config.global_config import GlobalConfig
 from hima.common.sdr import SparseSdr
-from hima.common.sds import Sds
-from hima.experiments.temporal_pooling.blocks.graph import Block
 from hima.experiments.temporal_pooling.data.synthetic_sequences import (
     generate_synthetic_sequences, generate_synthetic_single_element_sequences
 )
 
 
+@dataclasses.dataclass
 class Sequence:
     """Sequence of SDRs."""
     id: int
-    raw_sequence: list[SparseSdr]
+    seq: list[SparseSdr]
 
-    def __init__(self, id_: int, sequence):
-        self.id = id_
-        self.raw_sequence = sequence
-
-    def __iter__(self) -> Iterator[dict]:
-        for sdr in self.raw_sequence:
-            yield dict(input=sdr)
+    def __iter__(self) -> Iterator[SparseSdr]:
+        return iter(self.seq)
 
     def __len__(self):
-        return len(self.raw_sequence)
+        return len(self.seq)
 
 
-class SyntheticSequencesDatasetBlock(Block):
-    OUTPUT = 'output'
-
-    family = "generator"
-    supported_streams = {OUTPUT}
-
-    n_values: int
-    _sequences: list[Sequence]
-
-    def __init__(
-            self, id: int, name: str, n_values: int, sequences: list[Sequence],
-            values_sds: Sds
-    ):
-        super(SyntheticSequencesDatasetBlock, self).__init__(id, name)
-
-        self.n_values = n_values
-        self._sequences = sequences
-        self.register_stream(self.OUTPUT).resolve_sds(values_sds)
-
-    def reset(self, **kwargs):
-        super(SyntheticSequencesDatasetBlock, self).reset(**kwargs)
-
-    def build(self):
-        pass
-
-    def __iter__(self) -> Iterator[Sequence]:
-        return iter(self._sequences)
-
-    def compute(self, data: dict[str, SparseSdr], **kwargs):
-        ...
-
-    def reset_stats(self):
-        ...
-
-
-class SyntheticSequencesGenerator:
-    family = "generator"
-
+class SyntheticSequences:
+    alphabet_size: int
+    encoder: Any
+    n_sequences: int
     sequence_length: int
-    n_values: int
-    values_sds: Sds
-
-    sequence_similarity: float
-    sequence_similarity_std: float
-
-    seed: int
-    _rng: Generator
+    sequences: list[Sequence]
 
     def __init__(
             self, global_config: GlobalConfig,
-            sequence_length: int, n_values: int, active_size: int,
-            value_encoder: TConfig,
-            sequence_similarity: float,
-            seed: int,
-            sequence_similarity_std: float = 0.
+            n_sequences: int,
+            sequence_length: int,
+            alphabet_size: int,
+            encoder: TConfig,
+            **generator_kwargs: TConfig
     ):
-        self.sequence_length = sequence_length
-        self.n_values = n_values
-        self.value_encoder = global_config.resolve_object(
-            value_encoder,
-            n_values=self.n_values,
-            active_size=active_size,
-            seed=seed
-        )
-        self.values_sds = self.value_encoder.output_sds
+        self.alphabet_size = alphabet_size
+        self.encoder = global_config.resolve_object(encoder, n_values=alphabet_size)
 
-        self.sequence_similarity = sequence_similarity
-        self.sequence_similarity_std = sequence_similarity_std
-
-        self.seed = seed
-        self._rng = np.random.default_rng(seed)
-
-    def generate_sequences(self, n_sequences: int) -> list[Sequence]:
-        values_encoding = [self.value_encoder.encode(x) for x in range(self.n_values)]
-
-        if self.sequence_length > 1:
+        if sequence_length > 1:
             sequences = generate_synthetic_sequences(
-                n_sequences=n_sequences,
-                sequence_length=self.sequence_length,
-                n_values=self.n_values,
-                seed=self.seed,
-                sequence_similarity=self.sequence_similarity,
-                sequence_similarity_std=self.sequence_similarity_std,
+                n_sequences=n_sequences, sequence_length=sequence_length,
+                alphabet_size=alphabet_size, **generator_kwargs
             )
         else:
             sequences = generate_synthetic_single_element_sequences(
-                n_sequences=n_sequences, n_values=self.n_values, seed=self.seed
+                n_sequences=n_sequences, alphabet_size=alphabet_size,
+                **generator_kwargs
             )
 
-        encoded_sequences = [
-            Sequence(
-                id_=i_sequence,
-                sequence=[values_encoding[x] for x in sequence]
-            )
+        self.n_sequences = n_sequences
+        self.sequences = [
+            Sequence(id=i_sequence, seq=self.encoder.encode(sequence))
             for i_sequence, sequence in enumerate(sequences)
         ]
 
-        return encoded_sequences
-
-    def make_block(
-            self, block_id: int, block_name: str, sequences: list[Sequence]
-    ) -> SyntheticSequencesDatasetBlock:
-        return SyntheticSequencesDatasetBlock(
-            id=block_id, name=block_name,
-            n_values=self.n_values,
-            sequences=sequences,
-            values_sds=self.values_sds
-        )
--- hima/experiments/temporal_pooling/resolvers/type_resolver.py
+++ hima/experiments/temporal_pooling/resolvers/type_resolver.py
@@ -7,29 +7,59 @@ from hima.common.config.types import TTypeOrFactory, LazyTypeResolver
 
 
 class StpLazyTypeResolver(LazyTypeResolver):
-    def resolve(self, type_tag: str) -> TTypeOrFactory:
-        if type_tag == 'stp.synthetic_sequences':
-            from hima.experiments.temporal_pooling.test_stp import StpExperiment
-            return StpExperiment
-        if type_tag == 'storage':
-            from hima.experiments.temporal_pooling.blocks.graph import StorageBlock
-            return StorageBlock
-        if type_tag == 'synthetic_sequences':
-            from hima.experiments.temporal_pooling.blocks.dataset_synth_sequences import (
-                SyntheticSequencesGenerator
-            )
-            return SyntheticSequencesGenerator
-        if type_tag == 'int_bucket':
-            from hima.common.sdr_encoders import IntBucketEncoder
-            return IntBucketEncoder
-        if type_tag == 'int_random':
-            from hima.common.sdr_encoders import IntRandomEncoder
-            return IntRandomEncoder
-        if type_tag == 'custom_sp':
-            from hima.experiments.temporal_pooling.blocks.custom_sp import CustomSpatialPoolerBlock
-            return CustomSpatialPoolerBlock
-        if type_tag == 'concatenator':
-            from hima.experiments.temporal_pooling.blocks.concat import ConcatenatorBlock
-            return ConcatenatorBlock
+    def __init__(self):
+        super().__init__()
+        self.resolvers_by_prefix = [
+            ('block.', _resolve_block),
+            ('encoder.', _resolve_encoder),
+            ('stp.', _resolve_runner),
+            ('ds.', _resolve_dataset),
+        ]
 
-        raise ValueError(f'Unknown type tag: {type_tag}')
+    def resolve(self, type_tag: str) -> TTypeOrFactory:
+        assert type_tag is not None, 'Type is not specified'
+        resolved_type = None
+        for prefix, resolver in self.resolvers_by_prefix:
+            if type_tag.startswith(prefix):
+                resolved_type = resolver(type_tag)
+                break
+
+        if not resolved_type:
+            raise ValueError(f'Unknown type tag: {type_tag}')
+        return resolved_type
+
+
+def _resolve_block(type_tag: str):
+    if type_tag == 'block.storage':
+        from hima.experiments.temporal_pooling.blocks.graph import StorageBlock
+        return StorageBlock
+    if type_tag == 'block.custom_sp':
+        from hima.experiments.temporal_pooling.blocks.custom_sp import CustomSpatialPoolerBlock
+        return CustomSpatialPoolerBlock
+    if type_tag == 'block.concatenator':
+        from hima.experiments.temporal_pooling.blocks.concat import ConcatenatorBlock
+        return ConcatenatorBlock
+
+
+def _resolve_dataset(type_tag):
+    if type_tag == 'ds.synthetic_sequences':
+        from hima.experiments.temporal_pooling.blocks.dataset_synth_sequences import (
+            SyntheticSequences
+        )
+        return SyntheticSequences
+
+
+def _resolve_encoder(type_tag):
+    if type_tag == 'encoder.int_bucket':
+        from hima.common.sdr_encoders import IntBucketEncoder
+        return IntBucketEncoder
+    if type_tag == 'encoder.int_random':
+        from hima.common.sdr_encoders import IntRandomEncoder
+        return IntRandomEncoder
+
+
+def _resolve_runner(type_tag: str):
+    if type_tag == 'stp.synthetic_sequences':
+        from hima.experiments.temporal_pooling.test_stp import StpExperiment
+        return StpExperiment
+
