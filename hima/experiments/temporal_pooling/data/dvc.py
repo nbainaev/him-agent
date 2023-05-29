1544053f197d95d5f57243c7d6759f210bc5c7b1
Petr Kuderov <pkuderov@gmail.com>
2023-05-29 13:44:05 +0300

    optimize dvc dataset processing

--- hima/experiments/temporal_pooling/data/dvc.py
+++ hima/experiments/temporal_pooling/data/dvc.py
@@ -30,20 +30,32 @@ class DvcSequences:
             n_sequences: int,
             sequence_length: int,
             filepath: str,
+            seed: int,
+            sequential: bool = True,
     ):
         self.n_sequences = n_sequences
         dataset = self._read_dataset(filepath)
         self.sds = Sds.make(dataset.sds)
         sdrs = dataset.sdrs
         sdrs = sdrs[3000:-4000]
+        ds_size = len(sdrs)
 
-        self.sequences = [
-            Sequence(
-                id=i_sequence,
-                seq=sdrs[i_sequence*sequence_length:(i_sequence+1)*sequence_length]
+        self.sequences = []
+        i_sequence = 0
+        rng = np.random.default_rng(seed)
+        while len(self.sequences) < n_sequences or i_sequence > 100_000:
+            if sequential:
+                start = i_sequence*sequence_length % ds_size
+            else:
+                start = rng.integers(ds_size - sequence_length)
+            end = start + sequence_length
+            i_sequence += 1
+            if end >= ds_size:
+                continue
+
+            self.sequences.append(
+                Sequence(id=len(self.sequences), seq=sdrs[start:end])
             )
-            for i_sequence in range(n_sequences)
-        ]
 
     def __iter__(self):
         return iter(self.sequences)
