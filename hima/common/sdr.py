8b395ffb994b902fe897c97b707e7aa9c9d1b03f
Petr Kuderov <pkuderov@gmail.com>
2023-08-03 13:36:56 +0300

    extend sparse_to_dense common func

--- hima/common/sdr.py
+++ hima/common/sdr.py
@@ -3,6 +3,7 @@
 #  All rights reserved.
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+from __future__ import annotations
 
 from typing import Iterable, Union, Sequence
 
@@ -12,13 +13,15 @@ SparseSdr = Union[Iterable[int], Sequence[int], np.ndarray]
 DenseSdr = np.ndarray
 
 
-def sparse_to_dense(indices: SparseSdr, total_size: int) -> DenseSdr:
+def sparse_to_dense(indices: SparseSdr, total_size: int | tuple, dtype=int) -> DenseSdr:
     """Converts SDR from sparse representation to dense."""
-    # TODO: test int vs int8
-    dense_vector = np.zeros(total_size, dtype=np.int8)
+    dense_vector = np.zeros(total_size, dtype=dtype)
+    if dense_vector.ndim > 1:
+        dense_vector = dense_vector.flatten()
+
     dense_vector[indices] = 1
     return dense_vector
 
 
 def dense_to_sparse(dense_vector: DenseSdr) -> SparseSdr:
-    return np.nonzero(dense_vector)[0]
+    return np.flatnonzero(dense_vector)
