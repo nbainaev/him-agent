66bd6d0844e981dd3fa2cce3f10997fa3ca3cc73
Petr Kuderov <pkuderov@gmail.com>
2023-08-14 15:57:32 +0300

    split SP pinball and aai configs

--- hima/modules/baselines/lstm.py
+++ hima/modules/baselines/lstm.py
@@ -11,6 +11,96 @@ import numpy as np
 torch.autograd.set_detect_anomaly(True)
 
 
+class LstmLayer:
+    def __init__(
+            self,
+            n_obs_states,
+            n_hidden_states,
+            lr=2e-3,
+            seed=None
+    ):
+        self.n_obs_states = n_obs_states
+        self.n_hidden_states = n_hidden_states
+        self.lr = lr
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        self.lstm = LSTMWMUnit(
+            input_size=n_obs_states,
+            hidden_size=n_hidden_states
+        ).to(self.device)
+
+        self.prediction = torch.zeros(self.n_obs_states, device=self.device)
+        self.loss = None
+
+        self.loss_function = nn.BCELoss()
+        self.optimizer = optim.RMSprop(self.lstm.parameters(), lr=self.lr)
+
+        if seed is not None:
+            torch.manual_seed(seed)
+
+        self._rng = np.random.default_rng(seed)
+
+    def observe(self, obs, learn=True):
+        dense_obs = np.zeros(self.n_obs_states, dtype='float32')
+        dense_obs[obs] = 1
+
+        dense_obs = torch.from_numpy(dense_obs).to(self.device)
+
+        if learn:
+            if self.loss is None:
+                self.loss = self.loss_function(self.prediction, dense_obs)
+            else:
+                self.loss += self.loss_function(self.prediction, dense_obs)
+
+            self.prediction = self.lstm(dense_obs)
+        else:
+            with torch.no_grad():
+                self.prediction = self.lstm(dense_obs)
+
+    def predict(self):
+        with torch.no_grad():
+            self.prediction = self.lstm(dense_obs)
+
+    def reset(self):
+        self.optimizer.zero_grad()
+
+        if self.loss is not None:
+            self.loss.backward()
+            self.optimizer.step()
+            self.loss = None
+
+        self.prediction = torch.zeros(self.n_obs_states, device=self.device)
+        self.lstm.message = (
+            torch.zeros(self.n_hidden_states, device=self.device),
+            torch.zeros(self.n_hidden_states, device=self.device),
+        )
+
+    def n_step_prediction(self, initial_dist, steps, mc_iterations=100):
+        n_step_dist = np.zeros((steps, self.n_obs_states))
+        initial_message = (self.lstm.message[0].clone(), self.lstm.message[1].clone())
+
+        for i in range(mc_iterations):
+            dist_curr_step = initial_dist
+            for step in range(steps):
+                # sample observation from prediction density
+                gamma = self._rng.random(size=self.n_obs_states)
+                obs = np.flatnonzero(gamma < dist_curr_step)
+                dense_obs = np.zeros(self.n_obs_states, dtype='float32')
+                dense_obs[obs] = 1
+                dense_obs = torch.from_numpy(dense_obs).to(self.device)
+
+                # predict distribution
+                with torch.no_grad():
+                    prediction = self.lstm(dense_obs).cpu().detach().numpy()
+
+                n_step_dist[step] += 1/(i+1) * (prediction - n_step_dist[step])
+                dist_curr_step = prediction
+
+            self.lstm.message = initial_message
+
+        return n_step_dist
+
+
 class LSTMWMIterative:
     def __init__(
             self,
