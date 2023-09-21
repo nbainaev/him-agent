702ac13051742aabc6933d7363e684e52182e020
Evgenij Dzhivelikian <zhekauran@gmail.com>
2023-09-22 00:07:46 +0300

    chmm pinball experiment: fix bugs; refactor code; add metrics;

--- hima/experiments/successor_representations/runners/chmm.py
+++ hima/experiments/successor_representations/runners/chmm.py
@@ -45,67 +45,15 @@ class PinballTest:
         self.actions = conf['run']['actions']
         self.n_actions = len(self.actions)
 
-        # assembly agent
-        encoder_type = conf['run']['encoder']
-        encoder_conf = conf['encoder']
-        layer_conf = conf['layer']
-
-        if encoder_type == 'sp_ensemble':
-            from hima.modules.htm.spatial_pooler import SPDecoder, SPEnsemble
-
-            encoder_conf['seed'] = self.seed
-            encoder_conf['inputDimensions'] = list(self.raw_obs_shape)
-
-            encoder = SPEnsemble(**encoder_conf)
-            decoder = SPDecoder(encoder)
-            assert encoder.n_sp == 1
-
-            layer_conf['n_obs_states'] = encoder.sps[0].getNumColumns()
-            layer_conf['n_context_states'] = (
-                    encoder.sps[0].getNumColumns() * layer_conf['cells_per_column']
-            )
-        elif encoder_type == 'sp_grouped':
-            from hima.experiments.temporal_pooling.stp.sp_ensemble import (
-                SpatialPoolerGroupedWrapper
-            )
-            encoder_conf['seed'] = self.seed
-            encoder_conf['feedforward_sds'] = [self.raw_obs_shape, 0.1]
-
-            decoder_type = conf['run'].get('decoder', None)
-            decoder_conf = conf['decoder']
-
-            encoder = SpatialPoolerGroupedWrapper(**encoder_conf)
-            assert encoder.n_groups == 1
-
-            decoder = make_decoder(encoder, decoder_type, decoder_conf)
-
-            layer_conf['n_obs_states'] = encoder.getSingleNumColumns()
-            layer_conf['n_context_states'] = (
-                    encoder.getSingleNumColumns() * layer_conf['cells_per_column']
-            )
-        else:
-            raise ValueError(f'Encoder type {encoder_type} is not supported')
-
-        layer_conf['n_external_states'] = self.n_actions
-        layer_conf['seed'] = self.seed
-
-        layer = CHMMLayer(**layer_conf)
-
-        cortical_column = CorticalColumn(
-            layer,
-            encoder,
-            decoder
-        )
-
-        self.agent = BioHIMA(
-            cortical_column,
-            **conf['agent']
-        )
+        self.agent = self.load_agent(conf, conf['run'].get('agent_path', None))
 
         self.n_episodes = conf['run']['n_episodes']
         self.max_steps = conf['run']['max_steps']
         self.update_rate = conf['run']['update_rate']
         self.camera_mode = conf['run']['camera_mode']
+        self.reward_free = conf['run'].get('reward_free', False)
+        self.test_srs = conf['run'].get('test_srs', False)
+        self.test_sr_steps = conf['run'].get('test_sr_steps', 0)
 
         self.initial_previous_image = self._rng.random(self.raw_obs_shape)
         self.prev_image = self.initial_previous_image
@@ -119,7 +67,9 @@ class PinballTest:
                     'main_metrics/reward': np.sum,
                     'main_metrics/steps': np.mean,
                     'layer/surprise_hidden': np.mean,
-                    'agent/td_error': np.mean
+                    'agent/td_error': np.mean,
+                    'agent/test_sr_mse_approx_tail': np.mean,
+                    'agent/test_sr_mse': np.mean
                 },
                 self.logger
             )
@@ -127,7 +77,8 @@ class PinballTest:
             self.heatmap_metrics = HeatmapMetrics(
                 {
                     'agent/obs_rewards': np.mean,
-                    'agent/striatum_weights': np.mean
+                    'agent/striatum_weights': np.mean,
+                    'agent/real_rewards': np.mean
                 },
                 self.logger
             )
@@ -143,16 +94,11 @@ class PinballTest:
             )
 
     def run(self):
-        episode_print_schedule = 50
-
+        total_reward = np.zeros(self.raw_obs_shape).flatten()
         for i in range(self.n_episodes):
-            if i % episode_print_schedule == 0:
-                print(f'Episode {i}')
-
             steps = 0
             running = True
             action = -1
-            total_reward = 0
 
             self.prev_image = self.initial_previous_image
             self.environment.reset(self.start_position)
@@ -162,16 +108,20 @@ class PinballTest:
                 self.environment.step()
                 obs, reward, is_terminal = self.environment.obs()
                 running = not is_terminal
-                total_reward += reward
 
                 events = self.preprocess(obs, mode=self.camera_mode)
                 # observe events_t and action_{t-1}
                 pred_sr, gen_sr = self.agent.observe((events, action), learn=True)
-                self.agent.reinforce(total_reward)
+                self.agent.reinforce(reward)
+
+                total_reward[events] += reward
 
                 if running:
-                    # action = self._rng.integers(self.n_actions)
-                    action = self.agent.sample_action()
+                    if self.reward_free:
+                        action = self._rng.integers(self.n_actions)
+                    else:
+                        action = self.agent.sample_action()
+
                     # convert to AAI action
                     pinball_action = self.actions[action]
                     self.environment.act(pinball_action)
@@ -189,6 +139,25 @@ class PinballTest:
                             'agent/td_error': self.agent.td_error
                         }
                     )
+                    if self.test_srs:
+                        sr_mse_approx_tail, _, gen_sr_test_tail = self.compare_srs(
+                            self.test_sr_steps,
+                            True
+                        )
+                        sr_mse, pred_sr_test, gen_sr_test = self.compare_srs(
+                            self.test_sr_steps,
+                            False
+                        )
+                        self.scalar_metrics.update(
+                            {
+                                'agent/test_sr_mse_approx_tail': sr_mse_approx_tail,
+                                'agent/test_sr_mse': sr_mse
+                            }
+                        )
+                    else:
+                        pred_sr_test = None
+                        gen_sr_test = None
+                        gen_sr_test_tail = None
 
                     if (i % self.update_rate) == 0:
                         raw_beh = (self.prev_image * 255).astype('uint8')
@@ -235,6 +204,27 @@ class PinballTest:
                                     [raw_beh, proc_beh, pred_beh, pred_sr, gen_sr])
                             }
                         )
+
+                        if self.test_srs:
+                            self.image_metrics.update(
+                                {
+                                    'agent/sr': np.hstack(
+                                        [
+                                            raw_beh,
+                                            proc_beh,
+                                            (pred_sr_test.reshape(
+                                                self.raw_obs_shape
+                                            ) * 255).astype('uint8'),
+                                            (gen_sr_test.reshape(
+                                                self.raw_obs_shape
+                                            ) * 255).astype('uint8'),
+                                            (gen_sr_test_tail.reshape(
+                                                self.raw_obs_shape
+                                            ) * 255).astype('uint8')
+                                        ]
+                                    )
+                                }
+                            )
                 # <<< logging
 
                 steps += 1
@@ -258,7 +248,8 @@ class PinballTest:
                     self.heatmap_metrics.update(
                         {
                             'agent/obs_rewards': obs_rewards,
-                            'agent/striatum_weights': self.agent.striatum_weights
+                            'agent/striatum_weights': self.agent.striatum_weights,
+                            'agent/real_rewards': total_reward.reshape(self.raw_obs_shape)
                         }
                     )
                     self.heatmap_metrics.log(i)
@@ -283,6 +274,101 @@ class PinballTest:
         events = np.flatnonzero(diff > thresh)
 
         return events
+
+    def load_agent(self, conf=None, path=None):
+        if path is not None:
+            raise NotImplementedError
+        elif conf is not None:
+            # assembly agent
+            encoder_type = conf['run']['encoder']
+            encoder_conf = conf['encoder']
+            layer_conf = conf['layer']
+
+            if encoder_type == 'sp_ensemble':
+                from hima.modules.htm.spatial_pooler import SPDecoder, SPEnsemble
+
+                encoder_conf['seed'] = self.seed
+                encoder_conf['inputDimensions'] = list(self.raw_obs_shape)
+
+                encoder = SPEnsemble(**encoder_conf)
+                decoder = SPDecoder(encoder)
+                assert encoder.n_sp == 1
+
+                layer_conf['n_obs_states'] = encoder.sps[0].getNumColumns()
+                layer_conf['n_context_states'] = (
+                        encoder.sps[0].getNumColumns() * layer_conf['cells_per_column']
+                )
+            elif encoder_type == 'sp_grouped':
+                from hima.experiments.temporal_pooling.stp.sp_ensemble import (
+                    SpatialPoolerGroupedWrapper
+                )
+                encoder_conf['seed'] = self.seed
+                encoder_conf['feedforward_sds'] = [self.raw_obs_shape, 0.1]
+
+                decoder_type = conf['run'].get('decoder', None)
+                decoder_conf = conf['decoder']
+
+                encoder = SpatialPoolerGroupedWrapper(**encoder_conf)
+                assert encoder.n_groups == 1
+
+                decoder = make_decoder(encoder, decoder_type, decoder_conf)
+
+                layer_conf['n_obs_states'] = encoder.getSingleNumColumns()
+                layer_conf['n_context_states'] = (
+                        encoder.getSingleNumColumns() * layer_conf['cells_per_column']
+                )
+            else:
+                raise ValueError(f'Encoder type {encoder_type} is not supported')
+
+            layer_conf['n_external_states'] = self.n_actions
+            layer_conf['seed'] = self.seed
+
+            layer = CHMMLayer(**layer_conf)
+
+            cortical_column = CorticalColumn(
+                layer,
+                encoder,
+                decoder
+            )
+
+            conf['agent']['seed'] = self.seed
+            agent = BioHIMA(
+                cortical_column,
+                **conf['agent']
+            )
+        else:
+            raise ValueError
+
+        return agent
+
+    def compare_srs(self, sr_steps, approximate_tail):
+        current_state = self.agent.cortical_column.layer.internal_forward_messages
+        pred_sr = self.agent.predict_sr(current_state)
+        gen_sr = self.agent.generate_sr(
+            sr_steps,
+            initial_messages=current_state,
+            initial_prediction=self.agent.observation_messages,
+            approximate_tail=approximate_tail,
+        )
+
+        pred_sr = self.agent.cortical_column.decoder.decode(
+            normalize(
+                pred_sr.reshape(
+                    self.agent.cortical_column.layer.n_obs_vars, -1
+                )
+            ).flatten()
+        )
+        gen_sr = self.agent.cortical_column.decoder.decode(
+            normalize(
+                gen_sr.reshape(
+                    self.agent.cortical_column.layer.n_obs_vars, -1
+                )
+            ).flatten()
+        )
+
+        mse = np.mean(np.power(pred_sr - gen_sr, 2))
+
+        return mse, pred_sr, gen_sr
 
 
 class GridWorldTest:
