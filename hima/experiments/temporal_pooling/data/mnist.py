dd2161ff03a87bdb0b1a3c7c6ce1a8433bf90240
Petr Kuderov <pkuderov@gmail.com>
2023-11-10 07:35:11 +0300

    copy stp sweep config from server

--- hima/experiments/temporal_pooling/data/mnist.py
+++ hima/experiments/temporal_pooling/data/mnist.py
@@ -6,12 +6,18 @@
 import numpy as np
 from sklearn.datasets import load_digits
 
+from hima.common.sdr import SparseSdr
 from hima.common.sds import Sds
 
 
 class MnistDataset:
     images: np.ndarray
     target: np.ndarray
+
+    dense_sdrs: np.ndarray
+    sdrs: list[SparseSdr]
+
+    output_sds: Sds
 
     def __init__(self):
         self.digits = load_digits()
@@ -20,18 +26,23 @@ class MnistDataset:
 
         flatten_images: np.ndarray = self.images.reshape(self.n_images, -1)
         image_thresholds = np.mean(flatten_images, axis=-1, keepdims=True)
-        self.dense_sdrs = flatten_images >= image_thresholds
+        # noinspection PyUnresolvedReferences
+        self.dense_sdrs = (flatten_images >= image_thresholds).astype(int)
         self.sdrs = [np.flatnonzero(img) for img in self.dense_sdrs]
         self.output_sds = Sds(size=self.dense_sdrs.shape[-1], sparsity=self.dense_sdrs.mean())
 
         self.classes = [
             np.flatnonzero(self.target == cls)
-            for cls in range(10)
+            for cls in range(self.n_classes)
         ]
 
     @property
     def n_images(self):
         return self.images.shape[0]
+
+    @property
+    def n_classes(self):
+        return 10
 
     @property
     def image_shape(self):
