9f1d282fc0cc5b76cc619a21dc945b21fcb54c30
Evgenij Dzhivelikian <zhekauran@gmail.com>
2023-12-31 19:08:06 +0300

    scenario: refactor code

--- hima/common/scenario.py
+++ hima/common/scenario.py
@@ -19,34 +19,41 @@ class Scenario:
                 action = event['action']
                 params = event['params']
                 self.events.append(
-                    {'condition': condition, 'check_step': check_step, 'action': action, 'params': params,
-                     'done': False, 'last_check': None})
+                    {
+                        'condition': condition,
+                        'check_step': check_step,
+                        'action': action,
+                        'params': params,
+                        'done': False,
+                        'last_check': None
+                    }
+                )
 
     def check_conditions(self):
         for event in self.events:
-            step = self.get_attr(event['check_step'])
+            step = self._get_attr(event['check_step'])
             if (event['last_check'] != step) and not event['done']:
                 event['last_check'] = step
                 attr_name, operator, val, repeat = event['condition']
-                attr = self.get_attr(attr_name)
+                attr = self._get_attr(attr_name)
                 if operator == 'equal':
                     if attr == val:
-                        self.execute(event)
+                        self._execute(event)
                 elif operator == 'mod':
                     if (attr % val) == 0:
-                        self.execute(event)
+                        self._execute(event)
                 else:
                     raise NotImplemented(f'Operator "{operator}" is not implemented!')
 
-    def execute(self, event):
+    def _execute(self, event):
         method_name = event['action']
         params = event['params']
-        f = self.get_attr(method_name)
+        f = self._get_attr(method_name)
         f(**params)
         if event['condition'][-1] == 'norepeat':
             event['done'] = True
 
-    def get_attr(self, attr):
+    def _get_attr(self, attr):
         obj = self.runner
         for a in attr.split('.'):
             obj = getattr(obj, a)
