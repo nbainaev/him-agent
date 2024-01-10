3aa9a5bc4bed2e05116fa2d63feb1c96525f8544
Petr Kuderov <pkuderov@gmail.com>
2024-01-10 15:17:52 +0300

    add global vars

---
+++ hima/experiments/temporal_pooling/graph/global_vars.py
@@ -0,0 +1,15 @@
+#  Copyright (c) 2023 Autonomous Non-Profit Organization "Artificial Intelligence Research
+#  Institute" (AIRI); Moscow Institute of Physics and Technology (National Research University).
+#  All rights reserved.
+#
+#  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
+
+VARS_INPUT = 'input.sdr'
+VARS_STEP = 'step'
+VARS_STEP_FINISHED = 'step_finished'
+VARS_SEQUENCE_ID = 'sequence_id'
+VARS_SEQUENCE_FINISHED = 'step_finished'
+VARS_EPOCH = 'epoch'
+VARS_EPOCH_FINISHED = 'epoch_finished'
+VARS_TRACKING_ENABLED = 'tracking_enabled'
+VARS_LEARN = 'learn'
