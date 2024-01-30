fix RateSdr similarity computation

hima/experiments/temporal_pooling/stats/metrics.py
@@ -106,40 +106,39 @@ def _sdrr_similarity(
     """
     Optimized for SDRs represented with arrays. For fast computations, it utilizes
     a zeroed-out dense SDR array (will be cleared after using before returning the result).
-
-    NB: Both x1 and x2 are expected to have their sdr bits to have non-zero rates.
-        Otherwise, supp(...) calculation will be incorrect.
     """
+    if len(x1.sdr) == 0 or len(x2.sdr) == 0:
+        # we define that empty SDRs aren't similar to anything, even to each other
+        return 0.
+
     # print(f'x1: {x1}')
     # print(f'x2: {x2}')
     dense_cache[x2.sdr] = x2.values
-    dense_cache[x1.sdr] -= x1.values
+    norm = np.sum(x2.values)
 
-    # similarity is 1 - avg (L1 distance from x1 to x2) over supp(x2)
+    dense_cache[x1.sdr] -= x1.values
+    if symmetrical:
+        norm_ = np.sum(x1.values)
+
+    # we define non-symmetrical distance as: d(x1, x2) = |x1 - x2| / |x2|
     dense_cache[x2.sdr] = np.abs(dense_cache[x2.sdr])
     raw_distance = np.sum(dense_cache[x2.sdr])
-    # print(f'raw_distance: {raw_distance}')
-    norm = len(x2.sdr)
-    # print(f'norm: {norm}')
 
-    # clear cache; also make x1 -> x1 \ x2, which we use in `symmetrical` case below
-    dense_cache[x2.sdr] = 0
+    distance = safe_divide(raw_distance, norm)
 
     if symmetrical:
-        # similarity is 1 - avg (L1 distance from x1 to x2) over supp(x1 | x2)
-        # so, we also need to add (x1 \ x2) part
+        # symmetrical distance is an average of two non-symmetrical distances
         dense_cache[x1.sdr] = np.abs(dense_cache[x1.sdr])
-        raw_distance += np.sum(dense_cache[x1.sdr])
-        norm += np.count_nonzero(dense_cache[x1.sdr])
-        # print(f'raw_distance: {raw_distance}')
-        # print(f'norm: {norm}')
+        raw_distance = np.sum(dense_cache[x1.sdr])
+        distance = (distance + safe_divide(raw_distance, norm_)) / 2
 
-    # finish clearing cache
+    distance = max(min(distance, 1.), 0.)
+
+    # clear cache
+    dense_cache[x2.sdr] = 0
     dense_cache[x1.sdr] = 0
 
-    result = 1 - safe_divide(raw_distance, norm)
-    # print(f'result: {result}')
-    # assert False
+    result = 1 - distance
     return result
 
 