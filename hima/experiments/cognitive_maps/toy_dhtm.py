b34345a9e652724052eea381e4bba8a0bbb3dc80
Evgenij Dzhivelikian <zhekauran@gmail.com>
2024-07-25 15:28:09 +0300

    toy dhtm: add visual server communication; fix bugs

--- hima/experiments/cognitive_maps/toy_dhtm.py
+++ hima/experiments/cognitive_maps/toy_dhtm.py
@@ -4,6 +4,12 @@
 #
 #  Licensed under the AGPLv3 license. See LICENSE in the project root for license information.
 import numpy as np
+import socket
+import json
+from hima.modules.belief.utils import get_data, send_string, NumpyEncoder
+
+HOST = "127.0.0.1"
+PORT = 5555
 
 
 class ToyDHTM:
@@ -17,12 +23,17 @@ class ToyDHTM:
             n_obs_states,
             n_actions,
             n_clones,
-            consolidation_threshold: int = 1  # controls noise tolerance?
+            consolidation_threshold: int = 1,  # controls noise tolerance?
+            visualize: bool = False,
+            visualization_server=(HOST, PORT)
     ):
         self.n_clones = n_clones
         self.n_obs_states = n_obs_states
         self.n_actions = n_actions
         self.n_hidden_states = self.n_clones * self.n_obs_states
+        self.visualize = visualize
+        self.vis_server_address = visualization_server
+
         self.transition_counts = np.zeros(
             (self.n_actions, self.n_hidden_states, self.n_hidden_states)
         )
@@ -34,6 +45,10 @@ class ToyDHTM:
         self.action_buffer = list()
         self.state_buffer = list()
 
+        self.vis_server = None
+        if self.visualize:
+            self.connect_to_vis_server()
+
     def reset(self):
         self.clear_buffers()
 
@@ -43,23 +58,31 @@ class ToyDHTM:
         self.state_buffer.clear()
 
     def observe(self, obs_state, action):
+        # for debugging
+        # event type: (name: str, data: tuple)
+        events = dict()
+
         self.observation_buffer.append(obs_state)
         self.action_buffer.append(action)
         # state to be defined
         self.state_buffer.append(None)
 
         step = len(self.observation_buffer) - 1
+        pos = step
+        resolved = False
 
-        if step == 0:
-            # initial step
-            column_states = np.arange(self.n_clones) + obs_state
-            state = column_states[np.argmax(self.activation_counts[column_states])]
-            self.state_buffer.append(state)
-        else:
-            pos = step
-            resolved = False
-            # resolve conflicts
-            while not resolved:
+        events.update({'new_obs': (pos, obs_state, action)})
+
+        while not resolved:
+            if step == 0:
+                # initial step
+                column_states = np.arange(self.n_clones) + obs_state
+                state = column_states[np.argmax(self.activation_counts[column_states])]
+                self.state_buffer[pos] = state
+                resolved = True
+
+                events.update({'set_state': state})
+            else:
                 # input variables
                 obs_state = self.observation_buffer[pos]
                 column_states = np.arange(self.n_clones) + obs_state
@@ -86,6 +109,9 @@ class ToyDHTM:
                     ~permanence_mask
                 ]
 
+                events.update(
+                    {'predict_forward': (correct_prediction, wrong_perm, wrong_temp)}
+                )
                 # cases:
                 # 1. correct set is not empty
                 if len(correct_prediction) > 0:
@@ -95,11 +121,17 @@ class ToyDHTM:
                             self.activation_counts[correct_prediction]
                         )
                     ]
+                    self.state_buffer[pos] = state
                     resolved = True
+
+                    events.update({'set_state': state})
                 # 2. correct set is empty
                 else:
                     if state is None:
                         state = column_states[np.argmax(self.activation_counts[column_states])]
+                        self.state_buffer[pos] = state
+
+                        events.update({'set_state': state})
 
                     if len(wrong_perm) == 0:
                         resolved = True
@@ -112,6 +144,13 @@ class ToyDHTM:
                         column_states = np.arange(self.n_clones) + self.observation_buffer[pos - 1]
                         coincide = np.isin(sparse_prediction, column_states)
                         correct_prediction = sparse_prediction[coincide]
+
+                        events.update(
+                            {
+                                'predict_backward':
+                                (correct_prediction, sparse_prediction[~coincide])
+                            }
+                        )
 
                         if len(correct_prediction) > 0:
                             prev_state = correct_prediction[
@@ -131,14 +170,65 @@ class ToyDHTM:
 
                         self.state_buffer[pos - 1] = prev_state
 
-                        # move to previous position
-                        pos -= 1
-                        if pos == 0:
-                            resolved = True
+                        events.update({'set_prev_state': prev_state})
 
                 # in any case
-                self.state_buffer[pos] = state
-                self.transition_counts[prev_action, prev_state, wrong_temp] = 0
+                if len(wrong_temp) > 0:
+                    self.transition_counts[prev_action, prev_state, wrong_temp] = 0
+                    self.activation_counts[wrong_temp] -= 1
+
+                    events.update({'remove_con': (prev_state, wrong_temp)})
+
                 self.transition_counts[prev_action, prev_state, state] += 1
                 self.activation_counts[state] += 1
-                self.activation_counts[wrong_temp] -= 1
+
+                events.update({'reinforce_con': (prev_state, state)})
+                # move to previous position
+                if not resolved:
+                    pos -= 1
+
+                    events.update({'move': pos})
+
+                    if pos == 0:
+                        resolved = True
+
+            if self.vis_server is not None:
+                self._send_events(events)
+
+            events.clear()
+
+    def connect_to_vis_server(self):
+        self.vis_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.vis_server.connect(self.vis_server_address)
+            # handshake
+            self._send_json_dict({'type': 'hello'})
+            data = get_data(self.vis_server)
+            print(data)
+
+            if data != 'toy_dhtm':
+                raise socket.error(
+                    f'Handshake failed {self.vis_server_address}: It is not ToyDHTM vis server!'
+                )
+            print(f'Connected to visualization server {self.vis_server_address}!')
+        except socket.error as msg:
+            self.vis_server.close()
+            self.vis_server = None
+            print(f'Failed to connect to the visualization server: {msg}. Proceed.')
+
+    def _send_events(self, events):
+        data = get_data(self.vis_server)
+        if data == 'skip':
+            self._send_json_dict({'type': 'skip'})
+        elif data == 'close':
+            self.vis_server.close()
+            self.vis_server = None
+            print('Server shutdown. Proceed.')
+        elif data == 'step':
+            data_dict = {'type': 'events'}
+            data_dict.update(events)
+            self._send_json_dict(data_dict)
+
+    def _send_json_dict(self, data_dict):
+        send_string(json.dumps(data_dict, cls=NumpyEncoder), self.vis_server)
