80cd35fcacef24212195b2e1655b467328d16086
Petr Kuderov <pkuderov@gmail.com>
2024-11-17 20:57:56 +0300

    fix pruning support: pruned weights are zeroed and disabled from learning

--- hima/experiments/temporal_pooling/stp/se_dense.py
+++ hima/experiments/temporal_pooling/stp/se_dense.py
@@ -118,7 +118,11 @@ class SpatialEncoderDenseBackend:
         self.pruning_controller = None
         if pruning is not None:
             self.pruning_controller = PruningController(self, **pruning)
-            self.pruned_mask = np.zeros_like(self.weights, dtype=bool)
+        self.pruned_mask = np.zeros_like(self.weights, dtype=bool)
+        self.alive_connections = np.tile(
+            np.arange(self.ff_size, dtype=int),
+            (self.output_size, 1)
+        )
 
         print(
             f'Init SE backend: {self.lebesgue_p}-norm | {self.avg_radius:.3f}'
@@ -135,17 +139,19 @@ class SpatialEncoderDenseBackend:
         if sdr.size == 0:
             return
 
+        alive_connections = None if self.rf_sparsity == 1.0 else self.alive_connections
+
         # TODO: negative Xs is not supported ATM
         if self.learning_policy == LearningPolicy.KROTOV:
             if self.persistent_signs:
                 oja_krotov_kuderov_update(self.weights, sdr, x, u, y, lr)
             else:
-                oja_krotov_update(self.weights, sdr, x, u, y, lr)
+                oja_krotov_update(self.weights, sdr, x, u, y, lr, alive_connections)
         else:
             if self.persistent_signs:
                 willshaw_kuderov_update(self.weights, sdr, x, y, lr)
             else:
-                willshaw_update(self.weights, sdr, x, y, lr)
+                willshaw_update(self.weights, sdr, x, y, lr, alive_connections)
 
         if self.match_p != 1.0:
             self.weights_pow_p[sdr] = self.get_weight_pow_p(sdr)
@@ -161,6 +167,12 @@ class SpatialEncoderDenseBackend:
             return
 
         (sparsity, rf_size), t = timed(pc.shrink_receptive_field)(self.pruned_mask)
+        # update alive connections
+        self.alive_connections = np.array([
+            np.flatnonzero(~neuron_connections)
+            for neuron_connections in self.pruned_mask
+        ])
+
         stage = pc.stage
         sparsity_pct = round(100.0 * sparsity, 1)
         t = round(t * 1000.0, 2)
@@ -209,22 +221,25 @@ class SpatialEncoderDenseBackend:
 
 
 @jit()
-def willshaw_update(weights, sdr, x, y, lr):
+def willshaw_update(weights, sdr, x, y, lr, alive_connections):
     # Willshaw learning rule, L1 normalization:
     # dw = lr * y * (x - w)
 
     v = y * lr
     for ix, vi in zip(sdr, v):
         w = weights[ix]
-        w += vi * (x - w)
-        if vi < 0:
-            # only anti-hebbian learning causes sign change, if input x is always positive
-            # in this case, restrict updates
-            np.fmax(w, 0.0, w)
+
+        if alive_connections is None:
+            w += vi * (x - w)
+            fix_anti_hebbian_negatives(vi, w, None)
+        else:
+            m = alive_connections[ix]
+            w[m] += vi * (x[m] - w[m])
+            fix_anti_hebbian_negatives(vi, w, m)
 
 
 @jit()
-def oja_krotov_update(weights, sdr, x, u, y, lr):
+def oja_krotov_update(weights, sdr, x, u, y, lr, alive_connections):
     # Oja-Krotov learning rule, L^p normalization, p >= 2:
     # dw = lr * y * (x - u * w)
 
@@ -238,11 +253,15 @@ def oja_krotov_update(weights, sdr, x, u, y, lr):
     for ix, vi in zip(sdr, v):
         ui = u[ix]
         w = weights[ix]
-        w += vi * (x - ui * w)
-        if vi < 0:
-            # only anti-hebbian learning causes sign change, if input x is always positive
-            # in this case, restrict updates
-            np.fmax(w, 0.0, w)
+
+        if alive_connections is None:
+            w += vi * (x - ui * w)
+            fix_anti_hebbian_negatives(vi, w, None)
+        else:
+            m = alive_connections[ix]
+            w[m] += vi * (x[m] - ui * w[m])
+            fix_anti_hebbian_negatives(vi, w, m)
+
 
 
 @jit()
@@ -273,6 +292,19 @@ def oja_krotov_kuderov_update(weights, sdr, x, u, y, lr):
         w = weights[ix]
         w += vi * (np.sign(w) * x - ui * w)
 
+
+@jit()
+def fix_anti_hebbian_negatives(vi, w, mask):
+    # `vi` here is a value indicating hebbian (>0) or anti-hebbian (<0)
+    if vi >= 0:
+        return
+
+    # only anti-hebbian learning causes sign change, if input x is always positive
+    # in this case, restrict updates
+    if mask is None:
+        np.fmax(w, 0.0, w)
+    else:
+        w[mask] = np.fmax(w[mask], 0.0)
 
 @jit()
 def _get_scale(u):
